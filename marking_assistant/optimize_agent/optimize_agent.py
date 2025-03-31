4cf54762564d91b301d4aecef17f0c44a5beece4
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2025-03-31 15:25:03 +0100

    added optimize_agent.py script for running all evaluation steps.

--- marking_assistant/optimize_agent/optimize_agent.py
+++ marking_assistant/optimize_agent/optimize_agent.py
@@ -1,1 +1,9 @@
-# ToDo: call all iteration_x.py files asynchronously
+import os
+import subprocess
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+for i in range(11):
+    script_name = f"{this_dir}/iteration_{i}.py"
+    print(f"Running {script_name}...")
+    subprocess.run(["python", script_name], check=True)
+    print(f"Finished {script_name}\n")
