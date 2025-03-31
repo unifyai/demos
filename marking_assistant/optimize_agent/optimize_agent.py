04dc50627dce1a0275afc254dddea6ff8b9aafe6
djl11 <dan@unify.ai>
2025-03-31 17:09:17 +0100

    added command line argument to optimize_agent.py

--- marking_assistant/optimize_agent/optimize_agent.py
+++ marking_assistant/optimize_agent/optimize_agent.py
@@ -1,8 +1,17 @@
 import os
 import subprocess
+import argparse
+import unify
+
+unify.set_context("Evals", overwrite=True)
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
-for i in range(11):
+
+args = argparse.ArgumentParser()
+args.add_argument("--iteration", type=int, default=11)
+args = args.parse_args()
+
+for i in range(args.iteration):
     script_name = f"{this_dir}/iteration_{i}.py"
     print(f"Running {script_name}...")
     subprocess.run(["python", script_name], check=True)
