efa66b2e79027f8aa4297ef261ebb3c03412bfaf
djl11 <dan@unify.ai>
2025-04-16 14:08:14 -0700

    got browser use demo working, without unify logging for now.

---
+++ browser_use/main.py
@@ -0,0 +1,18 @@
+from langchain_openai import ChatOpenAI
+from browser_use import Agent
+from dotenv import load_dotenv
+load_dotenv()
+
+import asyncio
+
+llm = ChatOpenAI(model="gpt-4o")
+
+async def main():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=llm,
+    )
+    result = await agent.run()
+    print(result)
+
+asyncio.run(main())
