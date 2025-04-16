e5fa75d6e18b6b75d637f2c6fd9532c19b2e938f
djl11 <dan@unify.ai>
2025-04-16 14:11:43 -0700

    added traced decorator to browser use demo.

--- browser_use/main.py
+++ browser_use/main.py
@@ -3,10 +3,14 @@ from browser_use import Agent
 from dotenv import load_dotenv
 load_dotenv()
 
+import unify
 import asyncio
+
+unify.activate("Browser Use")
 
 llm = ChatOpenAI(model="gpt-4o")
 
+@unify.traced
 async def main():
     agent = Agent(
         task="Compare the price of gpt-4o and DeepSeek-V3",
